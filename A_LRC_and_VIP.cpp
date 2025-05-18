5c6560ad44bd787e316b2eeafe296195247612f3
aman4codes <aman4codes@gmail.com>
2025-05-18 23:02:46 +0530

    Solved

---
+++ A_LRC_and_VIP.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        bool found = false;
+
+        for (int j = 0; j < n; j++) {
+            int gcd_a = arr[j]; // group A: one element
+            int gcd_b = 0;      // group B: rest
+
+            for (int k = 0; k < n; k++) {
+                if (k != j)
+                    gcd_b = __gcd(gcd_b, arr[k]);
+            }
+
+            if (gcd_a != gcd_b) {
+                cout << "YES\n";
+                for (int k = 0; k < n; k++) {
+                    if (k == j)
+                        cout << "1 ";
+                    else
+                        cout << "2 ";
+                }
+                cout << "\n";
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            cout << "NO\n";
+        }
+    }
+
+    return 0;
+}
