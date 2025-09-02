6a9fe3e49801444e39efe615730020f8dd05519e
aman4codes <aman4codes@gmail.com>
2025-09-02 18:38:19 +0530

    Solved

--- A_Divisibility_Problem.cpp
+++ A_Divisibility_Problem.cpp
@@ -1,15 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main()
-{
+int fun() {
+    int a, b;
+    cin >> a >> b;
+
+    for (int j = 0; j <= a; j++) {
+        if (a % b == 0) {
+            return j;
+        }
+        a = a + 1;
+    }
+
+    return -1;
+}
+
+int main() {
     int t;
     cin >> t;
-    while (t--)
-    {
-        int a, b;
-        cin >> a >> b;
-        int i = (b - a % b) % b;
-        cout << i << endl;
+
+    for (int i = 1; i <= t; i++) {
+        cout << fun() << "\n";
     }
+
+    return 0;
 }
--- B_Swap_and_Delete.cpp
+++ B_Swap_and_Delete.cpp
@@ -1,75 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Function to find maximum number of consecutive 1s or 0s
-int max_consecutive_chars(const string &str) 
+void solve()
 {
-    int max_count = 1;
-    int current_count = 1;
-    for (int i = 1; i < str.length(); i++) 
+    // your code here
+    string s;
+    cin >> s;
+    long long int count_1 = 0;
+    long long int count_0 = 0;
+    long long int size_s = s.length();
+    for (int i = 0; i < size_s; i++)
     {
-        if (str[i] == str[i - 1]) 
+        if (s[i] == '0')
         {
-            current_count++;
-            max_count = max(max_count, current_count);
-        } 
-        else
-        {
-            current_count = 1;
-        }
-    }
-    return max_count;
-}
-
-// Function to print result based on the max streak
-void print_result(const string &str, int count_1, int count_0, int max_streak) 
-{   
-    if(count_1==0 || count_0==0)
-    {
-        if(count_1==1 || count_0==1)
-        {
-            cout<<"1"<<endl;
+            count_0++;
         }
         else
         {
-            cout<<max(count_1,count_0)-1<<endl;
+            count_1++;
         }
     }
-    else if(max_streak==2)
+    int size_t = 0;
+
+    if (count_0 == count_1)
     {
-        cout<<"1"<<endl;
+        cout << 0 << endl;
     }
-    else if (max_streak > (str.length() / 2)) 
+    else
     {
-        cout << max(count_1, count_0) << endl;
-    } 
-    else 
-    {
-        cout << abs(count_1 - count_0) << endl;
+        for (int j = 0; j <= size_s; j++)
+        {
+            if (s[j] == '0')
+            {
+                if (count_1 > 0)
+                {
+                    count_1--;
+                    size_t++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            else
+            {
+                if (count_0 > 0)
+                {
+                    count_0--;
+                    size_t++;
+                }
+                else
+                {
+                    break;
+                }
+
+            }
+        }
+
+        cout << size_s-size_t<< endl;
     }
 }
 
-int main() 
+int main()
 {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int t;
     cin >> t;
-    while (t--) 
+    while (t--)
     {
-        string str;
-        cin >> str;
-
-        int count_1 = count(str.begin(), str.end(), '1');
-        int count_0 = str.length() - count_1;
-
-        if (count_1 == count_0) 
-        {
-            cout << 0 << endl;
-        } 
-        else 
-        {
-            int max_streak = max_consecutive_chars(str);
-            print_result(str, count_1, count_0, max_streak);
-        }
+        solve();
     }
     return 0;
 }
