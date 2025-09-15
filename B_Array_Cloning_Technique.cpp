436a21c792a4182840862e3d8a1b66952eeb9da5
aman4codes <aman4codes@gmail.com>
2025-09-15 20:16:21 +0530

    Solved

--- B_Array_Cloning_Technique.cpp
+++ B_Array_Cloning_Technique.cpp
@@ -2,63 +2,71 @@
 
 using namespace std;
 
-void solve() 
+void solve()
 {
     int n;
     cin >> n;
-    map<int, int> freq;
-    int max_freq = 0;
-    for (int i = 0; i < n; ++i) 
+    vector<int> arr(n);
+    unordered_map<int, int> freq;
+    for (int i = 0; i < n; i++)
     {
-        int a;
-        cin >> a;
-        freq[a]++;
-        if (freq[a] > max_freq) 
+        cin >> arr[i];
+        freq[arr[i]]++;
+    }
+
+    int maxFreq = 0;
+    int element = -1;
+
+    for (auto &p : freq)
+    {
+        if (p.second > maxFreq)
         {
-            max_freq = freq[a];
+            maxFreq = p.second;
+            element = p.first;
         }
     }
 
-    if (max_freq == n) 
+    bool allSame = true;
+    for (int i = 1; i < n; i++)
     {
-        cout << 0 << std::endl;
-        return;
+        if (arr[i] != arr[0])
+        {
+            allSame = false;
+            break;
+        }
     }
 
-    int ops = 0;
-    int current_count = max_freq;
-    
-    // Initial state: We have one array with `max_freq` of the desired element.
-    // We need to fill `n - max_freq` more spots.
-    
-    while (current_count < n) 
+    int rem_size = n - maxFreq;
+    int ans = rem_size;
+    int i = 0;
+    if (!allSame)
     {
-        // Step 1: Clone the current state. Costs 1 operation.
-        ops++;
-        
-        // After cloning, we have `current_count` new elements to use.
-        // We can swap at most `current_count` elements into our target array.
-        // The number of spots we still need to fill is `n - current_count`.
-        
-        int elements_to_add = n - current_count;
-        int swaps_possible = current_count;
-        
-        int swaps_to_do = min(elements_to_add, swaps_possible);
-        
-        ops += swaps_to_do;
-        current_count += swaps_to_do;
+        while (true)
+        {
+            maxFreq *= 2;
+            i++;
+            if (maxFreq >= n)
+            {
+                break;
+            }
+        }
+
+        cout << ans + i << endl;
     }
-    
-    cout << ops << endl;
+    else
+    {
+        cout << 0 << endl;
+    }
 }
 
-int main() 
+int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         solve();
     }
     return 0;
